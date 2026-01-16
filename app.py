0e4eaea560da7f8c88971562f8d5d33f45e9808f
rbiswas101 <rbiswas101@gmail.com>
2026-01-16 15:55:38 +1100

    Update app.py
    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Use the route() decorator to tell Flask what URL should trigger the function
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!Webhook!</p>"
+    return "<p>Hello, World!Webhook!!</p>"
 
 # Run the application on a specific port (e.g., 8000)
 if __name__ == '__main__':
